37c1196f631558f688589934fed8ce695412d364
Balrasko <filiphromada.lasale@gmail.com>
2025-12-09 10:27:50 +0100

    editied

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,6 @@
 <script setup lang="ts">
 import socket from "src/services/socket"
 
-// ponecháš len základný connect log (ak ho chceš)
 socket.on('connect', () => {
   console.log('Connected to Socket.IO:', socket.id)
 })
